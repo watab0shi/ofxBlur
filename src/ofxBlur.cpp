9137d25f6836222f4242b5e1a3b37485786513ad
M-wataru-kani <wataru-kani@aquaring.co.jp>
2017-10-15 14:55:28 +0900

    cleanup

--- src/ofxBlur.cpp
+++ src/ofxBlur.cpp
@@ -1,252 +1,311 @@
 #include "ofxBlur.h"
 
-float Gaussian(float x, float mean, float variance) {
-	x -= mean;
-  return (1. / sqrt(TWO_PI * variance)) * exp(-(x * x) / (2 * variance));
+
+//----------------------------------------
+float Gaussian( float _x, float _mean, float _variance )
+{
+  _x -= _mean;
+  return ( 1. / sqrt( TWO_PI * _variance ) ) * exp( -( _x * _x ) / ( 2 * _variance ) );
 }
 
-void GaussianRow(int elements, vector<float>& row, float variance = .2) {
-	row.resize(elements);
-	for(int i = 0; i < elements; i++) {
-		float x = ofMap(i, 0, elements - 1, -1, 1);
-		row[i] = Gaussian(x, 0, variance);
-	}
+//----------------------------------------
+void GaussianRow( int _elements, vector< float >& _row, float _variance = .2 )
+{
+  _row.resize( _elements );
+  
+  for( int i = 0; i < _elements; ++i )
+  {
+    float x   = ofMap( i, 0, _elements - 1, -1, 1 );
+    _row[ i ] = Gaussian( x, 0, _variance );
+  }
 }
 
-string generateBlurSource(int radius, float shape) {
-	int rowSize = 2 * radius + 1;
-
-	// generate row
-	vector<float> row;
-	GaussianRow(rowSize, row, shape);
-
-	// normalize row and coefficients
-	vector<float> coefficients;
-	float sum = 0;
-	for(int i = 0; i < row.size(); i++) {
-		sum += row[i];
-	}
-	for(int i = 0; i < row.size(); i++) {
-		row[i] /= sum;
-	}
-	int center = row.size() / 2;
-	coefficients.push_back(row[center]);
-	for(int i = center + 1; i < row.size(); i += 2) {
-		float weightSum = row[i] + row[i + 1];
-		coefficients.push_back(weightSum);
-	}
-
-	// generate offsets
-	vector<float> offsets;
-	for(int i = center + 1; i < row.size(); i += 2) {
-		int left = i - center;
-		int right = left + 1;
-		float leftVal = row[i];
-		float rightVal = row[i + 1];
-		float weightSum = leftVal + rightVal;
-		float weightedAverage = (left * leftVal + right * rightVal) / weightSum;
-		offsets.push_back(weightedAverage);
-	}
-
-	stringstream src;
-    src << "#version 120\n";
-	src << "#extension GL_ARB_texture_rectangle : enable\n";
-	src << "uniform sampler2DRect source;\n";
-	src << "uniform vec2 direction;\n";
-	src << "void main(void) {\n";
-	src << "\tvec2 tc = gl_TexCoord[0].st;\n";
-	src << "\tgl_FragColor = " << coefficients[0] << " * texture2DRect(source, tc);\n";
-	for(int i = 1; i < coefficients.size(); i++) {
-		int curOffset = i - 1;
-		src << "\tgl_FragColor += " << coefficients[i] << " * \n";
-		src << "\t\t(texture2DRect(source, tc - (direction * " << offsets[i - 1] << ")) + \n";
-		src << "\t\ttexture2DRect(source, tc + (direction * " << offsets[i - 1] << ")));\n";
-	}
-	src << "}\n";
-
-	return src.str();
+//----------------------------------------
+string generateBlurSource( int _radius, float _shape )
+{
+  int rowSize = 2 * _radius + 1;
+  
+  // generate row
+  vector< float > row;
+  GaussianRow( rowSize, row, _shape );
+  
+  // normalize row and coefficients
+  vector< float > coefficients;
+  float sum = 0;
+  for( auto& r : row ) sum += r;
+  for( auto& r : row ) r   /= sum;
+  
+  int center = row.size() / 2;
+  coefficients.push_back( row[ center ] );
+  
+  for( int i = center + 1; i < row.size(); i += 2 )
+  {
+    float weightSum = row[ i ] + row[ i + 1 ];
+    coefficients.push_back( weightSum );
+  }
+  
+  // generate offsets
+  vector< float > offsets;
+  for( int i = center + 1; i < row.size(); i += 2 )
+  {
+    int   left            = i - center;
+    int   right           = left + 1;
+    float leftVal         = row[ i ];
+    float rightVal        = row[ i + 1 ];
+    float weightSum       = leftVal + rightVal;
+    float weightedAverage = ( left * leftVal + right * rightVal ) / weightSum;
+    offsets.push_back( weightedAverage );
+  }
+  
+  stringstream src;
+  src << "#version 120"                                                              << endl;
+  src << "#extension GL_ARB_texture_rectangle : enable"                              << endl;
+  src << "uniform sampler2DRect source;"                                             << endl;
+  src << "uniform vec2 direction;"                                                   << endl;
+  src << "void main(void) {"                                                         << endl;
+  src << "  vec2 tc = gl_TexCoord[0].st;"                                            << endl;
+  src << "  gl_FragColor = " << coefficients[ 0 ] << " * texture2DRect(source, tc);" << endl;
+  
+  for( int i = 1; i < coefficients.size(); ++i )
+  {
+    int curOffset = i - 1;
+    src << "  gl_FragColor += " << coefficients[ i ] << " * "                             << endl;
+    src << "    (texture2DRect(source, tc - (direction * " << offsets[ i - 1 ] << ")) + " << endl;
+    src << "     texture2DRect(source, tc + (direction * " << offsets[ i - 1 ] << ")));"  << endl;
+  }
+  src << "}"                                                                              << endl;
+  
+  return src.str();
 }
 
-string generateCombineSource(int passes, float downsample) {
-	vector<string> combineNames;
-	for(int i = 0; i < passes; i++) {
-		combineNames.push_back("s" + ofToString(i));
-	}
-	stringstream src;
-    src << "#version 120\n";
-	src << "#extension GL_ARB_texture_rectangle : enable\n";
-	src << "uniform sampler2DRect " << ofJoinString(combineNames, ",") << ";\n";
-	src << "uniform float brightness;\n";
-	if(downsample == 1) {
-		src << "const float scaleFactor = 1.;\n";
-	} else {
-		src << "const float scaleFactor = " << downsample << ";\n";
-	}
-	src << "void main(void) {\n";
-	src << "\tvec2 tc = gl_TexCoord[0].st;\n";
-	for(int i = 0; i < passes; i++) {
-		src << "\tgl_FragColor " << (i == 0 ? "=" : "+=");
-		src << " texture2DRect(" << combineNames[i] << ", tc);";
-		src << (i + 1 != passes ? " tc *= scaleFactor;" : "");
-		src << "\n";
-	}
-	src << "\tgl_FragColor *= brightness / " << passes << ".;\n";
-	src << "}\n";
-	return src.str();
+//----------------------------------------
+string generateCombineSource( int _passes, float _downsample )
+{
+  vector< string > combineNames;
+  for( int i = 0; i < _passes; ++i ) combineNames.push_back( "s" + ofToString( i ) );
+  
+  stringstream src;
+  src << "#version 120"                                                                     << endl;
+  src << "#extension GL_ARB_texture_rectangle : enable"                                     << endl;
+  src << "uniform sampler2DRect " << ofJoinString( combineNames, "," ) << ";"               << endl;
+  src << "uniform float brightness;"                                                        << endl;
+  src << "const float scaleFactor = " << ( ( _downsample == 1 ) ? 1. : _downsample ) << ";" << endl;
+  
+  src << "void main(void) {"                                                                << endl;
+  src << "  vec2 tc = gl_TexCoord[0].st;"                                                   << endl;
+  for( int i = 0; i < _passes; ++i )
+  {
+    src << "  gl_FragColor " << ( i == 0 ? " =" : "+=" );
+    src << " texture2DRect(" << combineNames[ i ] << ", tc);" << endl;
+    if( i + 1 != _passes ) src << "  tc *= scaleFactor;"      << endl;
+  }
+  src << "  gl_FragColor *= brightness / " << _passes << ".;" << endl;
+  src << "}";
+  
+  return src.str();
 }
 
+//----------------------------------------
 ofxBlur::ofxBlur()
-:scale(1)
-,rotation(0)
-,brightness(1) {
+:scale( 1 )
+,rotation( 0 )
+,brightness( 1 )
+{
 }
 
-void ofxBlur::setup(int width, int height, int radius, float shape, int passes, float downsample, bool useFloatTexture) {
-	string blurSource = generateBlurSource(radius, shape);
-	if(ofGetLogLevel() == OF_LOG_VERBOSE) {
-		cout << "ofxBlur is loading blur shader:" << endl << blurSource << endl;
-	}
-	blurShader.setupShaderFromSource(GL_FRAGMENT_SHADER, blurSource);
-	blurShader.linkProgram();
-
-	if(passes > 1) {
-		string combineSource = generateCombineSource(passes, downsample);
-		if(ofGetLogLevel() == OF_LOG_VERBOSE) {
-			cout << "ofxBlur is loading combine shader:" << endl << combineSource << endl;
-		}
-		combineShader.setupShaderFromSource(GL_FRAGMENT_SHADER, combineSource);
-		combineShader.linkProgram();
-	}
-
-    if(useFloatTexture)base.allocate(width, height, GL_RGB32F);
-    else base.allocate(width, height);
+//----------------------------------------
+void ofxBlur::setup( int _width, int _height, int _radius, float _shape, int _passes, float _downsample, bool _useFloatTexture )
+{
+  string blurSource = generateBlurSource( _radius, _shape );
+  
+  if( ofGetLogLevel() == OF_LOG_VERBOSE ) cout << "ofxBlur is loading blur shader:" << endl << blurSource << endl;
+  
+  blurShader.setupShaderFromSource( GL_FRAGMENT_SHADER, blurSource );
+  blurShader.linkProgram();
+  
+  if( _passes > 1 )
+  {
+    string combineSource = generateCombineSource( _passes, _downsample );
     
-    base.begin(); ofClear(0); base.end();
-
-    ofFbo::Settings settings;
-    settings.useDepth = false;
-    settings.useStencil = false;
-    settings.numSamples = 0;
-    ping.resize(passes);
-    pong.resize(passes);
-    for(int i = 0; i < passes; i++) {
-        ofLogVerbose() << "building ping/pong " << width << "x" << height;
-        settings.width = width;
-        settings.height = height;
-        ping[i].allocate(settings);
-        ping[i].begin(); ofClear(0); ping[i].end();
-        pong[i].allocate(settings);
-        pong[i].begin(); ofClear(0); pong[i].end();
-//        ping[i].setDefaultTextureIndex(i);
-//        pong[i].setDefaultTextureIndex(i);
-        width *= downsample;
-        height *= downsample;
+    if( ofGetLogLevel() == OF_LOG_VERBOSE ) cout << "ofxBlur is loading combine shader:" << endl << combineSource << endl;
+    
+    combineShader.setupShaderFromSource( GL_FRAGMENT_SHADER, combineSource );
+    combineShader.linkProgram();
+  }
+  
+  if( _useFloatTexture ) base.allocate( _width, _height, GL_RGB32F );
+  else                   base.allocate( _width, _height );
+  
+  base.begin();
+  {
+    ofClear( 0 );
+  }
+  base.end();
+  
+  ofFbo::Settings settings;
+  settings.useDepth   = false;
+  settings.useStencil = false;
+  settings.numSamples = 0;
+  
+  ping.resize( _passes );
+  pong.resize( _passes );
+  
+  for( int i = 0; i < _passes; ++i )
+  {
+    ofLogVerbose() << "building ping/pong " << _width << "x" << _height;
+    settings.width  = _width;
+    settings.height = _height;
+    
+    ping[ i ].allocate( settings );
+    ping[ i ].begin();
+    {
+      ofClear( 0 );
     }
+    ping[ i ].end();
+    
+    pong[ i ].allocate( settings );
+    pong[ i ].begin();
+    {
+      ofClear( 0 );
+    }
+    pong[ i ].end();
+    
+//    ping[ i ].setDefaultTextureIndex( i );
+//    pong[ i ].setDefaultTextureIndex( i );
+    
+    _width  *= _downsample;
+    _height *= _downsample;
+  }
 }
 
-void ofxBlur::setScale(float scale) {
-	this->scale = scale;
+//----------------------------------------
+void ofxBlur::begin()
+{
+  base.begin();
 }
 
-void ofxBlur::setRotation(float rotation) {
-	this->rotation = rotation;
-}
-
-void ofxBlur::setBrightness(float brightness) {
-	this->brightness = brightness;
-}
-
-void ofxBlur::begin() {
-	base.begin();
-}
-
-void ofxBlur::end() {
-	base.end();
-
-	ofPushStyle();
-	ofSetColor(255);
-
-	ofVec2f xDirection = ofVec2f(scale, 0).getRotatedRad(rotation);
-	ofVec2f yDirection = ofVec2f(0, scale).getRotatedRad(rotation);
-	for(int i = 0; i < ping.size(); i++) {
-		ofFbo& curPing = ping[i];
-		ofFbo& curPong = pong[i];
-
-		// resample previous result into ping
-		curPing.begin();
-		int w = curPing.getWidth();
-		int h = curPing.getHeight();
-		if(i > 0) {
-			ping[i - 1].draw(0, 0, w, h);
-		} else {
-			base.draw(0, 0, w, h);
-		}
-		curPing.end();
-
-		// horizontal blur ping into pong
-		curPong.begin();
-		blurShader.begin();
-		blurShader.setUniformTexture("source", curPing.getTexture(), 0);
-		blurShader.setUniform2f("direction", xDirection.x, xDirection.y);
-		curPing.draw(0, 0);
-		blurShader.end();
-		curPong.end();
-
-		// vertical blur pong into ping
-		curPing.begin();
-		blurShader.begin();
-		blurShader.setUniformTexture("source", curPong.getTexture(), 0);
-		blurShader.setUniform2f("direction", yDirection.x, yDirection.y);
-		curPong.draw(0, 0);
-		blurShader.end();
-		curPing.end();
-	}
-
-	// render ping back into base
-	if(ping.size() > 1) {
-		int w = base.getWidth();
-		int h = base.getHeight();
-
-        ofPlanePrimitive plane;
-        plane.set(w, h);
-        plane.mapTexCoordsFromTexture(ping[0].getTexture());
-
-		base.begin();
-		combineShader.begin();
-		for(int i = 0; i < ping.size(); i++) {
-			string name = "s" + ofToString(i);
-            combineShader.setUniformTexture(name.c_str(),
-                                            ping[i].getTexture(),
-                                            1 + i);
+//----------------------------------------
+void ofxBlur::end()
+{
+  base.end();
+  
+  ofPushStyle();
+  {
+    ofSetColor( 255 );
+    
+    ofVec2f xDirection = ofVec2f( scale, 0 ).getRotatedRad( rotation );
+    ofVec2f yDirection = ofVec2f( 0, scale ).getRotatedRad( rotation );
+    for( int i = 0; i < ping.size(); ++i )
+    {
+      ofFbo& curPing = ping[ i ];
+      ofFbo& curPong = pong[ i ];
+      
+      // resample previous result into ping
+      curPing.begin();
+      {
+        int w = curPing.getWidth();
+        int h = curPing.getHeight();
+        
+        if( i > 0 ) ping[ i - 1 ].draw( 0, 0, w, h );
+        else        base.draw( 0, 0, w, h );
+      }
+      curPing.end();
+      
+      // horizontal blur ping into pong
+      curPong.begin();
+      {
+        blurShader.begin();
+        {
+          blurShader.setUniformTexture( "source", curPing.getTexture(), 0 );
+          blurShader.setUniform2f( "direction", xDirection.x, xDirection.y );
+          curPing.draw( 0, 0 );
         }
-		combineShader.setUniform1f("brightness", brightness);
-        ofPushMatrix();
-        ofTranslate(w / 2, h / 2);
-        plane.draw();
-        ofPopMatrix();
-		combineShader.end();
-		base.end();
-	} else {
-		base.begin();
-		ping[0].draw(0, 0);
-		base.end();
-	}
-
-	ofPopStyle();
+        blurShader.end();
+      }
+      curPong.end();
+      
+      // vertical blur pong into ping
+      curPing.begin();
+      {
+        blurShader.begin();
+        {
+          blurShader.setUniformTexture( "source", curPong.getTexture(), 0 );
+          blurShader.setUniform2f( "direction", yDirection.x, yDirection.y );
+          curPong.draw( 0, 0 );
+        }
+        blurShader.end();
+      }
+      curPing.end();
+    }
+    
+    // render ping back into base
+    if( ping.size() > 1 )
+    {
+      int w = base.getWidth();
+      int h = base.getHeight();
+      
+      ofPlanePrimitive plane;
+      plane.set( w, h );
+      plane.mapTexCoordsFromTexture( ping[ 0 ].getTexture() );
+      
+      base.begin();
+      {
+        combineShader.begin();
+        {
+          for( int i = 0; i < ping.size(); ++i )
+          {
+            combineShader.setUniformTexture( "s" + ofToString( i ), ping[ i ].getTexture(), i + 1 );
+          }
+          combineShader.setUniform1f( "brightness", brightness );
+          
+          ofPushMatrix();
+          {
+            ofTranslate( w / 2, h / 2 );
+            plane.draw();
+          }
+          ofPopMatrix();
+        }
+        combineShader.end();
+      }
+      base.end();
+    }
+    else
+    {
+      base.begin();
+      {
+        ping[ 0 ].draw( 0, 0 );
+      }
+      base.end();
+    }
+  }
+  ofPopStyle();
 }
 
+//----------------------------------------
 ofTexture& ofxBlur::getTextureReference() {
   return getTexture();
 }
 
-ofTexture& ofxBlur::getTexture() {
-	return base.getTexture();
+//----------------------------------------
+void ofxBlur::draw( int _x, int _y, int _w, int _h )
+{
+  base.draw( _x, _y, _w, _h );
 }
 
-void ofxBlur::draw() {
-	base.draw(0, 0);
+//----------------------------------------
+void ofxBlur::draw( int _x, int _y )
+{
+  draw( _x, _y, base.getWidth(), base.getHeight() );
 }
 
-void ofxBlur::draw(ofRectangle rect) {
-    base.draw(rect);
+//----------------------------------------
+void ofxBlur::draw()
+{
+  draw( 0, 0 );
+}
+
+//----------------------------------------
+void ofxBlur::draw( ofRectangle _rect )
+{
+  draw( _rect.x, _rect.y, _rect.width, _rect.height );
 }
--- src/ofxBlur.h
+++ src/ofxBlur.h
@@ -2,31 +2,37 @@
 
 #include "ofMain.h"
 
-class ofxBlur {
+class ofxBlur
+{
 protected:
-	ofFbo base;
-	vector<ofFbo> ping, pong;
-
-	ofShader blurShader, combineShader;
-	float scale, rotation;
-	float downsample;
-	float brightness;
+  ofFbo           base;
+  vector< ofFbo > ping, pong;
+  
+  ofShader        blurShader, combineShader;
+  float           scale;
+  float           rotation;
+  float           downsample;
+  float           brightness;
+  
 public:
-	ofxBlur();
-
-	void setup(int width, int height, int radius = 32, float shape = .2, int passes = 1, float downsample = .5, bool useFloatTexture = false);
-
-	void setScale(float scale);
-	void setRotation(float rotation);
-	void setBrightness(float brightness); // only applies to multipass
-
-	void begin();
-	void end();
-	void draw();
-    void draw(ofRectangle rect);
-
-  OF_DEPRECATED_MSG("Use getTexture",ofTexture & getTextureReference());
-	ofTexture& getTexture();
+  ofxBlur();
+  
+  void setup( int _width, int _height, int _radius = 32, float _shape = .2, int _passes = 1, float _downsample = .5, bool _useFloatTexture = false );
+  
+  void setScale( float _scale ){ scale = _scale; }
+  void setRotation( float _rotation ){ rotation = _rotation; }
+  void setBrightness( float _brightness ){ brightness = _brightness; } // only applies to multipass
+  
+  void begin();
+  void end();
+  
+  void draw( int _x, int _y, int _w, int _h );
+  void draw( int _x, int _y );
+  void draw();
+  void draw( ofRectangle _rect );
+  
+  OF_DEPRECATED_MSG( "Use getTexture", ofTexture& getTextureReference() );
+  ofTexture& getTexture(){ return base.getTexture(); }
 };
 
 // <3 kyle
